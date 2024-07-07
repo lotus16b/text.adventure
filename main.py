a4056311472e3a8c27da546244f82880bde09777
Lotus <136942855+lotus16b@users.noreply.github.com>
2024-07-06 19:07:36 -0500

    main.py updates

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ pillChoice = input("> ")
 
 if(pillChoice == "blue pill"):
     print("You take the blue pill...you quickly become disoriented and as you stare blindly at the arcade cabinet...you blackout")
-    print("...and awake...feeling as if you are in motion, no longer in the Starlite Starbrite trailer park")
+    print("...and awake...feeling as if you are in motion, no longer in the Starlite Starbrite trailer court. You find yourself in a car? Or at least it looks like a car.")
+    print("except the scenery out the window is not California, it's a up close tour of the rings of Saturn.")
 elif(pillChoice == "red pill"):
-    print("You take the red pill and...feel no change...the arcade cabinet sits before you. You remain stuck in the Starlite Starbrite trailer park wishing you could do something with your life besides fixing Elvira's electric so she can watch her soaps.")
+    print("You take the red pill and...feel no change...the arcade cabinet sits before you. You remain stuck in the Starlite Starbrite trailer court wishing you could do something with your life besides fixing Elvira's electric so she can watch her soaps.")
 else:
     print("Invalid choice. Please enter blue pill or red pill...hint pick the blue pill.")
     

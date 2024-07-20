31fb4be394cbe0b0b9d0bdb415b4d5df9d06a5eb
Lotus <136942855+lotus16b@users.noreply.github.com>
2024-07-20 18:03:14 -0500

    test updates

--- test.py
+++ test.py
@@ -7,6 +7,8 @@ centauri_message = "Always trust Centauri, my boy."
 x, y, z = 32, 47, 171
 # create a list of equipment Alex needs
 equipment = ['starfighter uniform', 'helmet', 'gunstar', 'positive attitude']
+# create a list of high scores
+high_scores = [value**3 for value in range(99, 104)]
 # start the interaction with a question
 message = f"Hello, {full_name.title()}, how are you today?"
 print(message)
@@ -27,6 +29,10 @@ elif(response == "not good"):
     print(f"Just tap the crystals on the {equipment[-1]} and I'll be back.")
 else:
     print(f"hmmm....nevermind that...{centauri_message}")
+    print("You're the best I've ever seen, here are your high scores:")
+    print(high_scores)
+    print(f"That {max(high_scores)} set a record across the Star League!")
+    print("It is time to get going...") 
     print(f"You have {len(equipment)} items in your equipment list.")
     print("Here is a list of your equipment for fighting Xur:")
     print(', '.join(sorted(equipment)))
